5d02502cfe99540671a6993453c000fa6859c358
Ethan Water <watermonic@gmail.com>
2024-01-01 19:57:43 -0500

    documentation, better implementation

--- unwrap.go
+++ unwrap.go
@@ -1,10 +1,60 @@
+//NOTE: the functions in this library DO NOT work with functions that return multiple 
+//values. Much like in Rust, It can't directly handle multiple return values. If you have 
+//a Result or Option that contains a tuple or a struct with multiple values, you'd 
+//need to destructure or access those values explicitly.
+
+//The descriptions of the following methods are directly taken from the Rust's
+//standard library descriptions with slight modifications. You can find the 
+//original documentation in Rust here:
+//Result Doc: https://doc.rust-lang.org/stable/std/result/index.html
+//Option Doc: https://doc.rust-lang.org/stable/std/option/index.html
+
+//This library shouldn't be depended on for all error handling in production code.
+//Some of the functions use unsafe methods in order to handle its output. In situations 
+//where errors must be more explicitly handled, please use the default golang error
+//handling techniques. However, the functions below will be enough in MOST cases.
+//As of right now, only Result's unwrap methods are ported.
+
 package unwrap
 
 import (
 	"fmt"
 	"unsafe"
+	"reflect"
 )
 
+type Result[T any] struct {
+	Ok T
+	Err   error
+}
+
+//Checks if the Result type is valid and not an error
+func (result *Result[T]) IsOk() bool {
+	if result.Err != nil{
+		return false
+	}
+	return true
+}
+
+//Checks if the Result is an error
+func (result *Result[T]) IsErr() bool {
+	return !result.IsOk()
+}
+
+//Returns a Result type of functions output. This allows for most unwrap fucntions
+//to be called. 
+func Wrap[T any](value T, err error) Result[T] {
+	return Result[T]{Ok: value, Err: err}
+}
+
+//Returns the contained Ok value, consuming the self value.
+//Because this function may panic, its use is generally discouraged. Instead, 
+//prefer to use pattern matching and handle the error case explicitly, or call 
+//unwrap_or, unwrap_or_else, or unwrap_or_default.
+//
+//*Panics if the value is an Err, with a panic message provided by the Err’s value.
+///Usage
+//It can be called from a Result type or alone.
 func Unwrap[T any](value T, err error) T {
 	if err != nil {
 		panic(fmt.Errorf("failed to unwrap: %v", err))
@@ -12,36 +62,118 @@ func Unwrap[T any](value T, err error) T {
 	return value
 }
 
-func Unchecked[T any](output T, _ error) T {
+func (r Result[T]) Unwrap() T {
+	if r.Err != nil {
+		panic(fmt.Errorf("failed to unwrap: %v", r.Err))
+	}
+	return r.Ok
+}
+
+//Returns the contained Err value, consuming the self value.
+//
+//Panics if the value is an Ok, with a custom panic message provided by the Ok’s value.
+//
+//Usage
+//It can be called from a Result type or alone.
+func UnwrapErr[T any](ok T, err error) error {
+	if err == nil {
+		panic(fmt.Errorf("failed to unwrap: error is nil"))
+	}
+	return err
+}
+func (r Result[T]) UnwrapErr() error {
+	if r.Err == nil {
+		panic(fmt.Errorf("failed to unwrap: %v", r.Err))
+	}
+	return r.Err
+}
+
+
+//Returns the contained Err value, consuming the self value, without checking that the value is not an Ok.
+//
+//Safety
+//Calling this method on an Ok is undefined behavior.
+func UnwrapErrUnchecked[T any](_ T, err error) error {
+	return *(*error)(unsafe.Pointer(&err))
+}
+func (r Result[T])UnwrapErrUnchecked() error {
+	return *(*error)(unsafe.Pointer(&r.Err))
+}
+
+//Returns the contained Ok value or a provided default.
+//
+//Arguments passed to unwrap_or are eagerly evaluated; if you are passing the result 
+//of a function call, it is recommended to use UnwrapOrElse, which is lazily evaluated.
+func (r Result[T]) UnwrapOr(defaultValue T) T {
+	if r.Err != nil {
+		return defaultValue
+	}
+	return r.Ok
+}
+
+//TODO: Fix this
+//Returns the contained Ok value or computes it from a closure.
+//func (r Result[T]) UnwrapOrElse(handleErrorFunc func(T) T) T {
+//	if r.Err != nil {
+//		return handleErrorFunc(r.Ok)
+//	}
+//	return r.Ok
+//}
+
+
+//Returns the contained Ok value or a default
+//
+//Consumes the self argument then, if Ok, returns the contained value, otherwise 
+//if Err, returns the default value for that type.
+func (r Result[T]) UnwrapOrDefault() T {
+	//TODO: better handling of types
+	if r.Err != nil {
+		zero := reflect.Zero(reflect.TypeOf(r.Ok))
+		return zero.Interface().(T)
+	}
+	return r.Ok
+}
+
+//Returns the contained Ok value, consuming the self value, without checking that 
+//the value is not an Err.
+//
+//**Calling this method on an Err is undefined behavior.
+//
+///Usage
+//It can be called from a Result type or alone.
+func UnwrapUnchecked[T any](output T, _ error) T {
 	return *(*T)(unsafe.Pointer(&output))
 }
 
-type Result[T any] struct {
-	Value T
-	Err   error
+func (r Result[T]) UnwrapUnchecked() T {
+	return *(*T)(unsafe.Pointer(&r.Ok))
 }
 
-func Raw[T any](value T, err error) Result[T] {
-	return Result[T]{Value: value, Err: err}
-}
 
+//Returns the contained Ok value, consuming the self value.
+//
+//Because this function may panic, its use is generally discouraged. Instead, 
+//prefer to use pattern matching and handle the Err case explicitly, or call 
+//unwrap_or, unwrap_or_else, or unwrap_or_default.
+//
+//**Panics if the value is an Err, with a panic message including the passed message, 
+//and the content of the Err.
 func (r Result[T]) Expect(expect string) T {
 	if r.Err != nil {
 		panic(fmt.Errorf("%s : %v", expect, r.Err))
 	}
-	return r.Value
+	return r.Ok
 }
 
-func (r Result[T]) Or(defaultValue T) T {
-	if r.Err != nil {
-		return defaultValue
+//Returns the contained Err value, consuming the self value.
+//
+//Panics if the value is an Ok, with a panic message including the passed 
+//message, and the content of the Ok.
+func (r Result[T]) ExpectErr(msg string) error {
+	if r.Err == nil {
+		panic(fmt.Errorf("No error found: %v", msg))
 	}
-	return r.Value
+	return r.Err
 }
 
-func (r Result[T]) OrElse(handleErrorFunc func(T) T) T {
-	if r.Err != nil {
-		return handleErrorFunc(r.Value)
-	}
-	return r.Value
-}
+
--- unwrap_test.go
+++ unwrap_test.go
@@ -2,9 +2,99 @@ package unwrap
 
 import (
 	"errors"
-	"fmt"
 	"testing"
 )
+
+func TestUnwrap(t *testing.T) {
+	//testcase1
+	testcase1 := Unwrap(divide(4, 2))
+	if testcase1 != 2 {
+		t.Errorf("expected: %v, got: %v", 2, testcase1)
+	}
+
+	//testcase2
+	testcase2 := Wrap(divide(4,2)).Unwrap()
+	if testcase2 != 2 {
+		t.Errorf("expected: %v, got: %v", 2, testcase2)
+	}
+}
+
+func TestUnwrapErr(t *testing.T) {
+	//testcase1
+	if UnwrapErr(divide(4, 0)) == nil {
+		t.Errorf("failed")
+	}
+
+	//testcase2
+	if Wrap(divide(4,0)).UnwrapErr() == nil {
+		t.Errorf("failed")
+	}
+}
+
+func TestUnwrapErrUnchecked(t *testing.T) {
+	//testcase1
+	if UnwrapErrUnchecked(divide(4,0)) == nil {
+		t.Errorf("failed")
+	}
+	//testcase2
+	if Wrap(divide(4,0)).UnwrapErrUnchecked() == nil {
+		t.Errorf("failed")
+	}
+}
+
+func TestUnwrapOr(t *testing.T) {
+	result := Wrap(divide(4, 0)).UnwrapOr(2)
+	if result != 2 {
+		t.Errorf("Expected %d, got %d", 2, result)
+	}
+}
+
+//TODO: Fix this 
+//func TestUnwrapOrElse(t *testing.T) {
+//	var alternativeFunc func(int) int
+//	alternativeFunc = func(x int) int {
+//		return x^2
+//	}
+//
+//	result := Wrap(divide(4,0)).UnwrapOrElse(alternativeFunc)
+//
+//
+//}
+
+func TestUnwrapOrDefault(t *testing.T) {
+	result := Wrap(divide(4,0)).UnwrapOrDefault()
+	if result != 0 {
+		t.Errorf("Expected %d, got %d", 2, result)
+	}
+}
+
+func TestUnwrapUnchecked(t *testing.T) {
+	expected := 10
+	result := UnwrapUnchecked(expected, nil)
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+
+	expectedStr := "hello"
+	resultStr := UnwrapUnchecked(expectedStr, nil)
+	if resultStr != expectedStr {
+		t.Errorf("Expected %s, got %s", expectedStr, resultStr)
+	}
+}
+
+func TestExpect(t *testing.T) {
+	result := Wrap(divide(4, 2)).Expect("uh oh")
+	if result != 2 {
+		t.Errorf("Expected %d, got %d", 2, result)
+	}
+}
+
+func TestExpectErr(t *testing.T) {
+	result := Wrap(divide(4,0)).ExpectErr("uh oh")
+	if result == nil {
+		t.Errorf("failed")
+	}
+}
 
 func divide(a, b int) (int, error) {
 	if b == 0 {
@@ -12,55 +102,3 @@ func divide(a, b int) (int, error) {
 	}
 	return a / b, nil
 }
-
-func TestUnwrap(t *testing.T) {
-	result := Unwrap(divide(4, 2))
-	if result != 2 {
-		t.Errorf("expected: %v, got: %v", 2, result)
-	}
-}
-
-//func TestExpect(t *testing.T) {
-//	result := Raw(divide(4, 0)).Expect("uh oh")
-//	fmt.Println(result)
-//}
-
-func TestUnwrapOr(t *testing.T) {
-	result := Raw(divide(4, 0)).Or(2)
-	fmt.Println(result)
-}
-
-func TestUnchecked(t *testing.T) {
-	expected := 10
-	result := Unchecked(expected, nil)
-	if result != expected {
-		t.Errorf("Expected %d, got %d", expected, result)
-	}
-
-	expectedStr := "hello"
-	resultStr := Unchecked(expectedStr, nil)
-	if resultStr != expectedStr {
-		t.Errorf("Expected %s, got %s", expectedStr, resultStr)
-	}
-}
-
-//func TestOrElse(t *testing.T) {
-//handleErrorFunc := func(value int) int {
-//return value * 2
-//}
-
-//result := Raw(divide(4, 0)).OrElse(handleErrorFunc)
-
-//if result != 8 {
-//t.Errorf("Expected %d, got %d", 8, result)
-//}
-
-//err := errors.New("test error")
-//resWithError := Result[int]{Value: 10, Err: err}
-//expectedErrorValue := handleErrorFunc(resWithError.Value)
-//resultWithError := resWithError.OrElse(handleErrorFunc)
-
-//if resultWithError != expectedErrorValue {
-//t.Errorf("Expected %d, got %d", expectedErrorValue, resultWithError)
-//}
-//}
